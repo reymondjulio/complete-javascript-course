b26f90a51132faa804980e5eb495479a9081f78d
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 15:19:08 +0700

    feat: learn introduction javascript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+// Introduction Javascript
+
 let js = "amazing";
 if (js === "amazing") alert(`Javascript is FUN!`);
 
@@ -6,16 +8,3 @@ console.log(40 + 8 + 23 - 10);
 
 console.log("Reymond");
 console.log(23);
-
-let firstName = "Reymond";
-console.log(firstName);
-
-let LastName = "Julio";
-console.log(LastName);
-
-let reymond_julio = "RJ";
-console.log(reymond_julio);
-
-// 2 symbols that can declare variables
-let $function = 30;
-let _function = 5;
