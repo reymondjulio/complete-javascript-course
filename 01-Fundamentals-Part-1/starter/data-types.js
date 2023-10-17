3f346e4eeb1a9a1b6def58552ce5266e73909d05
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 15:24:52 +0700

    chore: add a data type file

---
+++ 01-Fundamentals-Part-1/starter/data-types.js
@@ -0,0 +1,17 @@
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof `Reymond`);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1990;
+console.log(typeof year);
+console.log(typeof null);
