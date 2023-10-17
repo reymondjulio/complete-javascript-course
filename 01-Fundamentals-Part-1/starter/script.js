e50cd65c6e37fe19d8027428581800dafbf70670
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 14:53:34 +0700

    feat: learn values & variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,19 @@ if (js === "amazing") alert(`Javascript is FUN!`);
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Reymond");
+console.log(23);
+
+let firstName = "Reymond";
+console.log(firstName);
+
+let LastName = "Julio";
+console.log(LastName);
+
+let reymond_julio = "RJ";
+console.log(reymond_julio);
+
+// 2 symbols that can declare variables
+let $function = 30;
+let _function = 5;
