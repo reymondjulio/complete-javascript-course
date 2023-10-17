98119d78089409c18444873ac5102e6e6a56921e
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 15:41:28 +0700

    feat: learn let, const, variables

---
+++ 01-Fundamentals-Part-1/starter/let-const-var.js
@@ -0,0 +1,14 @@
+// Let, Const, Var
+
+let age = 26;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastname = "Julio";
+console.log(lastname);
