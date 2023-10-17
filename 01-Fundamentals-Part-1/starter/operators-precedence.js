217f7f76178e561f70288c71da9dda7339de686a
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 16:37:38 +0700

    feat: learn operators precedence

---
+++ 01-Fundamentals-Part-1/starter/operators-precedence.js
@@ -0,0 +1,12 @@
+const yearNow = 2023;
+const ageReymond = yearNow - 1997;
+const ageJulio = yearNow - 2002;
+
+console.log(yearNow - 1997 > yearNow - 2002);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageReymond + ageJulio) / 2;
+console.log(ageReymond, ageJulio, averageAge);
