7673952c0ca03dc587fc392ded70e04c6fd28093
Reymond Julio <reymondjulio58@gmail.com>
2023-10-17 15:24:09 +0700

    chore: add a variable file

---
+++ 01-Fundamentals-Part-1/starter/variables.js
@@ -0,0 +1,14 @@
+// Values and Variables
+
+let firstName = "Reymond";
+console.log(firstName);
+
+let LastName = "Julio";
+console.log(LastName);
+
+let reymond_julio = "RJ";
+console.log(reymond_julio);
+
+// 2 symbols that can declare variables
+let $function = 30;
+let _function = 5;
