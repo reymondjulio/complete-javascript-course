fe49d63d2e817b14f3cf6cf2b4c57b602ff23891
Reymond Julio <reymondjulio58@gmail.com>
2023-10-26 16:39:45 +0700

    feat: learn objects

---
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -0,0 +1,11 @@
+// Objects
+
+const reymondArray = ["Reymond", "Julio", 2023 - 1997, "Programmer", ["Michael", "Peter", "Steven"]];
+
+const reymond = {
+  firstName: "Reymond",
+  lastName: "Julio",
+  age: 2023 - 1997,
+  job: "Programmer",
+  friends: ["Michael", "Peter", "Steven"],
+};
