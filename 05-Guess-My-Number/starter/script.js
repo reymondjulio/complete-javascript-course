47e4038fa0ef397b8d834e6bc93ef9cb7588335c
Reymond Julio <reymondjulio58@gmail.com>
2023-11-06 16:27:21 +0700

    feat: learn DOM manipulating css styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -25,6 +25,8 @@ document.querySelector('.check').addEventListener('click', function () {
   // When player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🍕 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
 
   // When guess is too high
