8fadfef8eba2b3c7b1f15efbadc35fb812baec44
Reymond Julio <reymondjulio58@gmail.com>
2023-11-07 16:04:55 +0700

    feat: implementing the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('#current--0');
@@ -16,23 +18,33 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
-
   const dice = Math.trunc(Math.random() * 6) + 1;
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1: if true,
   if (dice !== 1) {
     // Add dice to current score
     currentScore = currentScore + dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   }
   // switch to next player
   else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
